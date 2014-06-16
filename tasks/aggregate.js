821dbf325af42381e07f9d7aaf43951889acb48a
Aaron Bedra <aaron@aaronbedra.com>
2014-06-16 18:01:24 -0500

    One last refactoring of aggregate.js

--- tasks/aggregate.js
+++ tasks/aggregate.js
@@ -14,34 +14,22 @@ var members = github.users.map(function(user) {
   return user.name;
 });
 
-
-var aggregate = function(usersData) {
-  var data = {
-    connected_users: []
-  };
-
-  data.connected_users = users.connectedUsers(usersData);
-  data.similar_users = users.similarUsers(usersData);
-  data.repositories = repositories.all(usersData);
-  data.usage = usage.gather(usersData);
-
-  return data;
-};
-
-var appendOrganization = function(data) {
-  data.name = organization.name;
-  data.username = organization.username;
-  data.avatar_url = organization.avatar_url;
-  data.members = github.users;
-};
-
 async.map(members, osrc.fetch, function(error, results) {
   if (error) {
     return console.log('Error when mapping OSRC: ' + error.stack);
   }
 
-  var aggregateData = aggregate(results);
-  var finalData = appendOrganization(aggregateData);
-  fs.writeFileSync(__dirname + '/../data/data.json', JSON.stringify(aggregateData, null, 2));
+  var data = {};
+
+  data.connected_users = users.connectedUsers(results);
+  data.similar_users = users.similarUsers(results);
+  data.repositories = repositories.all(results);
+  data.usage = usage.gather(results);
+  data.name = organization.name;
+  data.username = organization.username;
+  data.avatar_url = organization.avatar_url;
+  data.members = github.users;
+
+  fs.writeFileSync(__dirname + '/../data/data.json', JSON.stringify(data, null, 2));
   console.log('Wrote data/data.json!');
 });
