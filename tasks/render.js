d3301fdc1b7dbaf9e8dbe7ed171fef99400f0709
Trek Glowacki <trek@groupon.com>
2014-06-16 11:45:05 -0500

    Just some notes for when we come back post-lunch

--- tasks/render.js
+++ tasks/render.js
@@ -1,11 +1,12 @@
 var Handlebars = require('handlebars');
 
-var template = "<h1>{{hello}}</h1>";
+var template = "<h1>{{hello}}</h1>"; // fs.readSync(thetempalte)
 var render = Handlebars.compile(template);
 
-var html = render({hello: 'world'});
+var data = {} // fs.readSync(some.json). Maybe manipulate it?
+var html = render(data);
 
-console.log(html);
+console.log(html); // fs.writeSync(filename, html).
 
 
 
