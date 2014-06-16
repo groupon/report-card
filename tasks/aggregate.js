048a0f4aa4e88bb74321858d9fb7d31be552cc0e
Sean Massa <smassa@groupon.com>
2014-06-16 13:09:29 -0500

    setup stub data and expand aggregate to include org info

--- tasks/aggregate.js
+++ tasks/aggregate.js
@@ -3,15 +3,27 @@ var fs = require('fs');
 var _ = require('underscore');
 var request = require('request');
 
+var read = function(projectRelativePath){
+  var fullPath = __dirname + '/../' + projectRelativePath;
+  return JSON.parse(fs.readFileSync(fullPath).toString());
+};
+
 // TODO
 var users = ['trek', 'endangeredmassa'];
 
 var getOsrc = function(username, callback){
   request.get('http://osrc.dfm.io/'+username+'.json', function(error, response){
+    var body = null;
+
     if (error) {
-      return callback(error);
+      console.log('Error hitting osrc.dfm.io: ' + error.message);
+      console.log('Defaulting to local stub for ' + username);
+      body = read('data/stub/'+username+'.json');
+    } else {
+      body = response.body;
     }
-    var json = JSON.parse(response.body);
+
+    var json = JSON.parse(body);
     callback(null, json);
   });
 };
@@ -48,12 +60,21 @@ var aggregate = function(usersData) {
   return data;
 };
 
+var addOrgData = function(data){
+  var usersData = read('data/users.json');
+  data.name = usersData.org.name;
+  data.username = usersData.org.username;
+  data.avatar_url = usersData.org.avatar_url;
+};
+
 async.map(users, getOsrc, function(error, results){
   if (error) {
     return console.log('Error when mapping OSRC: ' + error.stack);
   }
 
   var aggregateData = aggregate(results);
+  var finalData = addOrgData(aggregateData);
   fs.writeFileSync(__dirname + '/../data/data.json', JSON.stringify(aggregateData, null, 2));
+  console.log('Wrote data/data.json!');
 });
 
