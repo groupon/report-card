45d026728102434e2b4dddcc9818b4aa3fa5c259
Ashish Dixit <tundal45@gmail.com>
2014-06-16 17:05:44 -0500

    Add ability to sort events & language data, find out top language & top two activity.

--- tasks/render.js
+++ tasks/render.js
@@ -4,6 +4,23 @@ var fs = require('fs');
 var template = fs.readFileSync(__dirname + '/../templates/index.hbs').toString();
 var render = Handlebars.compile(template);
 
+var EVENT_TYPE_TO_ACTION_PHRASE = {
+  "PushEvent": "pushing code",
+  "CommitCommentEvent": "commenting on pull requests",
+  "CreateEvent": "creating repos",
+  "DeleteEvent": "removing repos",
+  "FollowEvent": "following others",
+  "ForkEvent": "forking repos",
+  "GistEvent": "creating/updating gists",
+  "GollumEvent": "creating/updating wiki docs",
+  "IssueCommentEvent": "commenting on issues",
+  "IssuesEvent": "responding to issues",
+  "PageBuildEvent": "building github pages",
+  "PullRequestEvent": "opening pull requests",
+  "PullRequestReviewCommentEvent": "commenting on pull requests",
+  "ReleaseEvent": "making releases",
+  "WatchEvent": "watching repos"
+}
 
 Handlebars.registerHelper('ceilingPercent', function(users){
   var percents = users.map(function(u){return u.percent});
@@ -23,7 +40,10 @@ Handlebars.registerHelper('sumEvents', function(events) {
 });
 
 Handlebars.registerHelper('eventTypeToName', function(type) {
-  return type.replace("Event", "");
+  type = type.replace("Event", "");
+  type = type.replace("IssueComment", "Issue Comment");
+  type = type.replace("CommitComment", "Commit Comment");
+  return type;
 });
 
 Handlebars.registerHelper('languageToPerson', function(language, number) {
@@ -46,8 +66,28 @@ Handlebars.registerHelper('languageToPerson', function(language, number) {
 });
 
 var data = JSON.parse(fs.readFileSync(__dirname + '/../data/data.json').toString());
-debugger;
-console.log(data.languages);
+
+
+data.usage.languages = data.usage.languages.sort(function(a, b) {
+  if (a.count > b.count)
+    return -1;
+  if (a.count < b.count)
+    return 1;
+  return 0;
+});
+
+data.usage.events = data.usage.events.sort(function(a, b) {
+  if (a.total > b.total)
+    return -1;
+  if (a.total < b.total)
+    return 1;
+  return 0;
+});
+
+data.topLanguage = data.usage.languages[0].language;
+data.topActivity = EVENT_TYPE_TO_ACTION_PHRASE[data.usage.events[0].type];
+data.secondTopActivity = EVENT_TYPE_TO_ACTION_PHRASE[data.usage.events[1].type];
+
 var html = render(data);
 
 fs.writeFileSync(__dirname + '/../public/index.html', html);
