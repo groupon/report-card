a912f42d8866965ae518918620832e693dc22463
Sean Massa <smassa@groupon.com>
2014-06-16 12:46:31 -0500

    add org info to users.json

--- tasks/users.js
+++ tasks/users.js
@@ -1,21 +1,43 @@
+var async = require('async');
 var fs = require('fs');
 var _ = require('underscore');
 var github = require('octonode');
 var client = github.client();
 var org = client.org('groupon');
 
-var writeUsers = function(error, members) {
-  var data = [];
+var getUsers = function(callback){
+  org.members(function(error, members) {
+    if (error) {
+      return callback(error);
+    }
 
-  if (error) {
-    console.log("Error: " + error);
-    return;
-  }
+    var users = [];
 
-  _.each(members, function(member) {
-    data.push(member["login"]);
+    _.each(members, function(member) {
+      users.push(member["login"]);
+    });
+
+    callback(null, users);
   });
+};
 
+var getInfo = function(callback){
+  org.info(function(error, info){
+    if (error) {
+      return callback(error);
+    }
+
+    var orgInfo = {
+      name: info.name,
+      username: info.login,
+      avatar_url: info.avatar_url
+    };
+
+    callback(null, orgInfo);
+  });
+};
+
+var writeData = function(data){
   fs.writeFile(__dirname + "/../data/users.json", JSON.stringify(data, null, 2), function(error) {
     if (error) {
       console.log("Error writing users: " + err);
@@ -23,6 +45,17 @@ var writeUsers = function(error, members) {
       console.log("Saved users to users.json");
     }
   });
-}
+};
 
-org.members(writeUsers);
+async.parallel([getUsers, getInfo], function(error, results){
+  var users = results[0];
+  var info = results[1];
+
+  var data = {
+    users: users,
+    org: info
+  };
+
+  writeData(data);
+});
+
