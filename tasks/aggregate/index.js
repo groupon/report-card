c20b7d622b6839cacc9d74fd6002041bd156a6ae
Sean Massa <smassa@groupon.com>
2014-06-17 14:43:14 -0500

    add lanyrd info to aggregate

--- tasks/aggregate/index.js
+++ tasks/aggregate/index.js
@@ -3,6 +3,7 @@ var _ = require('underscore');
 
 var pollOsrc = require('./osrc');
 var pollStackExchange = require('./stack');
+var lanyrd = require('./lanyrd');
 
 var fs = require('./fs-json.js');
 var github = fs.read('data/users.json');
@@ -11,18 +12,26 @@ var members = github.users.map(function(user) {
   return user.name;
 });
 
-var sift = function(mapping, keys){
+var sift = function(mapping, keys, finalKey){
   return _.compact(
     _.map(keys, function(key){
-      return mapping[key.toLowerCase()];
-  })).join(';');
+      var value = mapping[key.toLowerCase()]
+      if(value)
+        return value[finalKey];
+      else
+        return null;
+  }));
 };
 
-var mapping = fs.read('data/user-map.json').githubToStackExchange;
-var stackUserIds = sift(mapping, members);
+var mapping = fs.read('data/user-map.json').mapping;
+
+var stackUserIds = sift(mapping, members, 'stackexchange').join(';');
+var lanyrdIds = sift(mapping, members, 'lanyrd');
+
 var tasks = {
   stackExchange: _.partial(pollStackExchange, stackUserIds),
-  osrc: _.partial(pollOsrc, github, members)
+  osrc: _.partial(pollOsrc, github, members),
+  lanyrd: _.partial(lanyrd.getSpeakerCounts, lanyrdIds)
 };
 
 async.parallel(tasks, function(error, results){
@@ -31,6 +40,7 @@ async.parallel(tasks, function(error, results){
   var data = results.osrc;
   data.answers = results.stackExchange.answers;
   data.badges = results.stackExchange.badges;
+  data.talks = results.lanyrd;
 
   fs.write('data/data.json', data);
   console.log('Wrote data/data.json!');
