3fe50e9ab2ba3437faf44b16ca64d8ef40183111
Trek Glowacki <trek.glowacki@gmail.com>
2014-06-19 19:50:15 -0500

    Fix sizes of repo avatar so it's not fuzzy
    Also, use all 4 of the avatar hosts for more concurrent downloads

--- tasks/render/helpers/avatar.js
+++ tasks/render/helpers/avatar.js
@@ -1,4 +1,13 @@
+var assetHost = [null, "1", "2" ,"3"], assetHostIndex = 0;
 module.exports = function(username, size){
   size = size || 30;
-  return "https://avatars.githubusercontent.com/"+username+"?size="+size;
-}
+  var avartUrl = "https://avatars"+ (assetHost[assetHostIndex] || '') +".githubusercontent.com/"+username+"?size="+size;
+
+  if (assetHostIndex >= 3) {
+    assetHostIndex = 0;
+  } else {
+    assetHostIndex++;
+  }
+
+  return avartUrl;
+};
